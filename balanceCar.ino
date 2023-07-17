16fd6c1c2417390ea9cf75aa4aead6bc48374593
ZionLieb <1838169875@qq.com>
2023-07-17 14:25:43 +0800

    修改了balanceCar文件的内容，链接到了GetSpeed模块

--- balanceCar.ino
+++ balanceCar.ino
@@ -1,10 +1,11 @@
 #define PWMA 10
+
 #define PWMB 9
 #define AIN1 12
 #define AIN2 13
 #define BIN1 7
 #define BIN2 6
-
+#include "GetSpeed.ino"
 void setup() {
   pinMode(PWMA, OUTPUT);
   pinMode(PWMB, OUTPUT);
@@ -19,6 +20,7 @@ void loop() {
   digitalWrite(AIN2, LOW);  // 设置电机A的方向
   digitalWrite(BIN1, HIGH); // 设置电机A的方向
   digitalWrite(BIN2, LOW);  // 设置电机A的方向
+  unsigned long leftspd = get_speed_left();
   analogWrite(PWMA, 128); // 设置电机A的速度，128是一半速度
   analogWrite(PWMB, 128);
 
