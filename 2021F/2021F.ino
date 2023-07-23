c72fe523305d468d9d8ee0428b579145103acdfc
ZionLieb <1838169875@qq.com>
2023-07-23 17:20:31 +0800

    针对电赛F题小车实现了多个运动功能

---
+++ 2021F/2021F.ino
@@ -0,0 +1,93 @@
+#include "move.h"
+#include "GetSpeed.h"
+#include "PID.h"
+
+void setup(){
+    Serial.begin(9600);
+    motorInit();
+    setupSpeedTest();
+    PID_setup();
+}
+/*
+function: get the signal from the serial port and control the car,
+There are 7 commands:
+a:forward
+b:turn left
+c:turn right
+d:turn around
+e:stop
+q:navigate to the left
+r:navigate to the right
+
+all move functions are in the move.h
+move will got a direction, from 1 to 7.
+speed is currently not used.
+
+*/
+uint8_t direction=0;
+void loop()
+{
+    while(Serial.available())
+    {
+        char Receive_Data = Serial.read();
+        switch (Receive_Data)
+        {
+            case 'a':
+                Serial.println("1RUN");
+                direction = 1;
+                move(1,1,0);
+                break;
+            case 'b':
+                Serial.println("2LEFT");
+                direction = 2;
+                move(2,1,1000);
+                break;
+            case 'c':
+                Serial.println("3RIGHT");
+                direction = 3;
+                move(3,1,1000);
+                break;
+            case 'd':
+                Serial.println("4AROUND");
+                direction = 4;
+
+                move(4,1,500);
+                Stop();
+                delay(500);
+                move(4,1,500);
+                break;
+            case 'e':
+                Serial.println("5STOP");
+                direction = 5;
+                move(5,1,0);
+                break;
+            case 'q':
+                Serial.println("6left");
+                direction = 6;
+                move(6,1,100);
+                break;
+            case 'r':
+                Serial.println("7right");
+                direction = 7;
+                move(7,1,100);
+                break;
+
+        }
+    }
+        if(direction == 1)adjustSpeed();
+        // else if (Receive_Data=='i'){
+        //     Serial.println("6 SPEED-1");
+        //     if(speed>-8){
+        //         speed = speed - 1;
+        //     }else{speed = -8;}
+        //     //初值80+10*1,每次减10,最低0,则可以减9次,防溢出
+        // }else if (Receive_Data=='j'){
+        //     Serial.println("7 SPEED+1");
+        //     if(speed<16){
+        //         speed = speed + 1;}
+        //     else{speed = 16;}
+        //     //初值90，每次加10，最高255，则可以加16次,防溢出
+
+        // move(direction,speed,0);
+        //getSpeed();
+}
---
+++ 2021F/move.h
@@ -0,0 +1,88 @@
+#ifndef _MOVE_H
+#define _MOVE_H
+#include <stdint.h>
+#define PWMLeft 9
+#define PWMRight 10
+#define RightIn1 7
+#define RightIn2 6
+#define LeftIn1 12
+#define LeftIn2 13
+//definations of the fuctions to control the motors
+#define LeftForward() digitalWrite(LeftIn1,LOW);digitalWrite(LeftIn2,HIGH)
+#define LeftBackward() digitalWrite(LeftIn1,HIGH);digitalWrite(LeftIn2,LOW)
+#define RightForward() digitalWrite(RightIn1,LOW);digitalWrite(RightIn2,HIGH)
+#define RightBackward() digitalWrite(RightIn1,HIGH);digitalWrite(RightIn2,LOW)
+#define LeftStop() digitalWrite(LeftIn1,LOW);digitalWrite(LeftIn2,LOW)
+#define RightStop() digitalWrite(RightIn1,LOW);digitalWrite(RightIn2,LOW)
+//definations of the fuctions to control the car
+#define Stop() LeftStop();RightStop()
+//modify the mode by commenting the code
+//mode 1 : one go, one stop
+// #define forward(speed) LeftForward();RightForward();analogWrite(PWMLeft,speed);analogWrite(PWMRight,speed)
+// #define backward(speed) LeftBackward();RightBackward();analogWrite(PWMLeft,speed);analogWrite(PWMRight,speed)
+// #define left(speed) LeftStop();RightForward();analogWrite(PWMRight,speed)
+// #define right(speed) LeftForward();RightStop();analogWrite(PWMLeft,speed)
+//mode 2 : one speed is less then the other
+#define forward(speed) LeftForward();RightForward();analogWrite(PWMLeft,speed);analogWrite(PWMRight,speed)
+#define backward(speed) LeftBackward();RightBackward();analogWrite(PWMLeft,speed);analogWrite(PWMRight,speed)
+#define left(speed) LeftBackward();RightForward();analogWrite(PWMLeft,speed);analogWrite(PWMRight,speed)
+#define right(speed) LeftForward();RightBackward();analogWrite(PWMRight,speed);analogWrite(PWMLeft,speed)
+#define leftForward(speed) LeftForward();RightForward();analogWrite(PWMLeft,speed-30);analogWrite(PWMRight,speed)
+#define rightForward(speed) LeftForward();RightForward();analogWrite(PWMRight,speed-30);analogWrite(PWMLeft,speed)
+void move(uint8_t direction,uint8_t speed, uint8_t time);
+void motorInit();
+/*
+Description: a move function
+@param: direction: 0:forward 1:backward 2:left 3:right 4:stop
+@param: speed: 0~50
+@param: time: 0~255,if time=0, then move forever
+*/
+void move(uint8_t direction,uint8_t speed, uint8_t time){
+    // if (speed ==0){
+    //     Stop();
+    //     return;
+    // }
+    speed = 80 + speed*10;
+    //Serial.print('hello');
+    switch(direction){
+        case 1:
+            forward(speed);
+            break;
+        case 2:
+            left(speed);
+            break;
+        case 3:
+            right(speed);
+            break;
+        case 4:
+            left(speed);
+            break;
+        case 5:
+            Stop();
+            break;
+        case 6:
+            leftForward(speed);
+            break;
+        case 7:
+            rightForward(speed);
+            break;
+        default:
+            break;
+    }
+    if(time!=0){
+        delay(time);
+        Stop();
+    }
+
+}
+
+void motorInit(){
+    pinMode(PWMLeft,OUTPUT);
+    pinMode(PWMRight,OUTPUT);
+    pinMode(LeftIn1,OUTPUT);
+    pinMode(LeftIn2,OUTPUT);
+    pinMode(RightIn1,OUTPUT);
+    pinMode(RightIn2,OUTPUT);
+    Stop();
+}
+#endif
